a8d9d896cc04fb2ce08431bc4c8187f290b2bdf7
tp.joo <tp.joo@daum.net>
2022-07-11 19:30:08 +0900

    ggd revision

--- ggd.c
+++ ggd.c
@@ -9,16 +9,18 @@ int 	main(void)
 
 
 #if 0
+
 	for(i=2; i<=MAX_GGD_NUM; i+=4)
 	{
 		printf(" ---%dDAN----    ---%dDAN----    ---%dDAN----    ---%dDAN---- \n", i, i+1, i+2, i+3);
 		for(j=1; j<=MAX_GGD_NUM; j++)
 		{
 			printf("%2d X %2d = %2d   %2d X %2d = %2d   %2d X %2d = %2d   %2d X %2d = %2d \n", 
-						i, j, MUL(i,j),  i+1, j, MUL(i+1,j), i+2, j, MUL(i+2,j), i+3, j, MUL(i+3,j) );
+					i, j, MUL(i,j),  i+1, j, MUL(i+1,j), i+2, j, MUL(i+2,j), i+3, j, MUL(i+3,j) );
 		}
 		printf("--------------------------------------------------------- \n");
 	}
+
 #else
 
 	i = 2;
@@ -28,7 +30,7 @@ int 	main(void)
 		j = 1;
 		do {
 			printf("%2d X %2d = %2d   %2d X %2d = %2d   %2d X %2d = %2d   %2d X %2d = %2d \n", 
-						i, j, MUL(i,j),  i+1, j, MUL(i+1,j), i+2, j, MUL(i+2,j), i+3, j, MUL(i+3,j) );
+					i, j, MUL(i,j),  i+1, j, MUL(i+1,j), i+2, j, MUL(i+2,j), i+3, j, MUL(i+3,j) );
 			j++;
 		} while(j<=MAX_GGD_NUM);
 		printf("--------------------------------------------------------- \n");
